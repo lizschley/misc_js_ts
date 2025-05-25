925499ae4d6d00ae1194e8904600a53cf137b9b4
lizschley <7227538+lizschley@users.noreply.github.com>
2025-05-25 18:01:02 -0400

    Add a sort by date method

--- appscript_methods/monthly_script.js
+++ appscript_methods/monthly_script.js
@@ -4,6 +4,23 @@ function onEdit(e){
     budget.dropdown(a1);
   }
 
+  function onOpen() {
+    const ss = SpreadsheetApp.getActive();
+    const menu = [{ name: 'Sort By Date', functionName: 'sort_date'}]
+    ss.addMenu('Custom', menu);
+  }
+
+  function sort_date() {
+    const ss = SpreadsheetApp.getActive();
+    const expense_sheet = ss.getSheetByName('expenses');
+    let month_range = expense_sheet.getRange(2,1,expense_sheet.getLastRow()-1,5)
+    month_range.sort([
+      {column: 3, ascending: true}
+    ]);
+    date_range = expense_sheet.getRange('C2:C');
+    date_range.sort({column: 3, ascending: true});
+  }
+
   // DO NOT RUN THIS UNLESS THIS IS A COPIED SPREADSHEET
   // IT WILL DESTROY DATA
   function clean_prior_data(){
