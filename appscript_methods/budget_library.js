5f96d2c53e49c2fb2229ab52143ccd0730507896
lizschley <7227538+lizschley@users.noreply.github.com>
2026-01-03 11:35:41 -0500

    not sure what happened but this is what is in apps script

--- appscript_methods/budget_library.js
+++ appscript_methods/budget_library.js
@@ -29,17 +29,43 @@ function input_to_range_helper({sheet_name, a1_notation, one_cell_only=true, exp
   };
 }
 
-function set_date_validation(expense_sheet) {
-    const range = expense_sheet.getRange("C2:C");
-    const rule = SpreadsheetApp.newDataValidation()
-        .requireDate()
-        .build();
-    range.setDataValidation(rule);
+// this is run from the library and is the first step of starting a new monthly spreadsheet
+// MAKE SURE variables have the correct values
+function copy_monthly_file() {
+  library_values = getLibraryValues()
+  // Logger.log(JSON.stringify(library_values)); // Log the values to the Apps Script log
+  const folder_id = getValue('folder_id', library_values)
+  const copy_id = getValue('copy_id', library_values)
+  const new_name = getValue('new_name', library_values)
+  ensure_unique_name(folder_id, new_name)
+  DriveApp.getFileById(copy_id).makeCopy(new_name, DriveApp.getFolderById(folder_id))
 }
 
-function formatDate(expense_sheet) {
-    let column = expense_sheet.getRange("C2:C"); // Column C
-    column.setNumberFormat("MM/dd/yyyy");
+function getLibraryValues() {
+  const library_spreadsheet = SpreadsheetApp.getActive();
+	return library_spreadsheet.getSheetByName('variables').getDataRange().getValues();
+}
+
+function getValue(key, library_values) {
+  for (let idx = 0; idx < library_values.length; idx++) {
+    if (library_values[idx][0] == key) return library_values[idx][1]
+  }
+  return null
+}
+
+// this and the functions it calls are initiated manually from monthly spreadsheet
+function clean_old_data(ss, spreadsheet_id, name) {
+  if (ss.getId() != spreadsheet_id) {
+    throw Error('wrong spreadsheet')
+  }
+  SpreadsheetApp.setActiveSpreadsheet(ss);
+  const expense_sheet = ss.getSheetByName('expenses');
+  delete_rows(expense_sheet)
+  set_category_validation(ss, expense_sheet)
+  set_date_validation(expense_sheet)
+  formatDate(expense_sheet)
+  formatCurrency(expense_sheet)
+  add_annuals(ss, expense_sheet, name)
 }
 
 function test_annuals(ss, name) {
@@ -97,52 +123,34 @@ function add_annuals(ss, expense_sheet, name) {
     range.setValues(row)
     target_row++
   }
+  // Logger.log('date == ' + date)
+  // Logger.log('annual rows == ' + data_rows)
+  // Logger.log('size of rows == ' + data_rows.length)
+  // Logger.log(`annuals_sheet.getLastRow()-1: ${annuals_sheet.getLastRow()-1}`)
+  // Logger.log(`annuals_sheet.getLastColumn(): ${annuals_sheet.getLastColumn()}`)
 }
-
-function add_annuals(ss, expense_sheet, name) {
-const annuals_sheet = ss.getSheetByName('annuals');
-const data_rows = annuals_sheet.getRange(2,1,annuals_sheet.getLastRow()-1,annuals_sheet.getLastColumn()).getValues();
-const date = make_date(name)
-let target_row = 2
-var num_rows = data_rows.length;
-var num_cols = data_rows[0].length + 1;
-for (let idx = 0; idx < data_rows.length; idx++) {
-    number = data_rows[idx][2]/12
-    row = [[data_rows[idx][0], data_rows[idx][1], date, number]]
-    // Logger.log(row)
-    range = expense_sheet.getRange(target_row, 1, 1, row[0].length);
-    range.setValues(row)
-    target_row++
-}
-// Logger.log('date == ' + date)
-// Logger.log('annual rows == ' + data_rows)
-// Logger.log('size of rows == ' + data_rows.length)
-// Logger.log(`annuals_sheet.getLastRow()-1: ${annuals_sheet.getLastRow()-1}`)
-// Logger.log(`annuals_sheet.getLastColumn(): ${annuals_sheet.getLastColumn()}`)
-}
-
 
 function make_date(name) {
-const months = ["January", "February", "March", "April","May", "June", "July", "August","September", "October", "November", "December"];
-let temp = name.split('_')
-year = temp[0]
-month = temp[1]
-let month_num = months.indexOf(month) + 1;
-return `${month_num}/1/${temp[0]}`;
+  const months = ["January", "February", "March", "April","May", "June", "July", "August","September", "October", "November", "December"];
+  let temp = name.split('_')
+  year = temp[0]
+  month = temp[1]
+  let month_num = months.indexOf(month) + 1;
+  return `${month_num}/1/${temp[0]}`;
 }
 
 function ensure_unique_name(folder_id, new_name) {
-    let folder = DriveApp.getFolderById(folder_id); // Replace with your folder ID
-    let files = folder.getFiles();
-    while (files.hasNext()) {
-        var file = files.next();
-        if (file.getMimeType() === "application/vnd.google-apps.spreadsheet") {
-            existing_name = file.getName()
-            if (existing_name == new_name) {
-                throw new Error(`A file with the name ${new_name} already exists in the folder with id == ${folder_id}`);
-            }
-        }
+  let folder = DriveApp.getFolderById(folder_id); // Replace with your folder ID
+  let files = folder.getFiles();
+  while (files.hasNext()) {
+    var file = files.next();
+    if (file.getMimeType() === "application/vnd.google-apps.spreadsheet") {
+      existing_name = file.getName()
+      if (existing_name == new_name) {
+        throw new Error(`A file with the name ${new_name} already exists in the folder with id == ${folder_id}`);
+      }
     }
+  }
 }
 
 function dynamic_named_ranges_from_headers(in_notation='', in_sheet_name='dropdowns'){
